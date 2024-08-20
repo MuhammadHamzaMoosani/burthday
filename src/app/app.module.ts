dc4a791af3c8d2e3cf8477b94cf8acdcfc7405c4
MuhammadHamzaMoosani <hamzamoosani@hotmail.com>
2024-08-20 21:22:10 +0500

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomepageComponent } from './homepage/homepage.component';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { QuestionsComponent } from './questions/questions.component';
+import { LetterComponent } from './letter/letter.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    QuestionsComponent,
+    LetterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule, 
+    RouterModule.forRoot([
+      { path: "", component: HomepageComponent },
+      { path: "questions", component: QuestionsComponent },
+      {path:"letters", component:LetterComponent }
+    ])
   ],
   providers: [
     provideClientHydration()
